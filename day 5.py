6f4aa7e9daf2f6db9f8b13a8d85cecdd2ceb17d1
Huib van Nieuwenhuijze <Huib.van.Nieuwenhuijze@gmail.com>
2023-12-05 09:04:21 +0100

    Add day 5 test input

--- day 5.py
+++ day 5.py
@@ -4,7 +4,41 @@ import datetime
 import re
 
 # Test input
-input = [""]
+input = [
+"seeds: 79 14 55 13",
+"",
+"seed-to-soil map:",
+"50 98 2",
+"52 50 48",
+"",
+"soil-to-fertilizer map:",
+"0 15 37",
+"37 52 2",
+"39 0 15",
+"",
+"fertilizer-to-water map:",
+"49 53 8",
+"0 11 42",
+"42 0 7",
+"57 7 4",
+"",
+"water-to-light map:",
+"88 18 7",
+"18 25 70",
+"",
+"light-to-temperature map:",
+"45 77 23",
+"81 45 19",
+"68 64 13",
+"",
+"temperature-to-humidity map:",
+"0 69 1",
+"1 0 69",
+"",
+"humidity-to-location map:",
+"60 56 37",
+"56 93 4"
+]
 
 # input integers separated by commas
 #input= [int(i) for i in open("input_'+os.path.basename(__file__).split(".")[0]+'.txt').read().split(",")]
@@ -14,6 +48,9 @@ input = [""]
 
 # input complete lines
 #input = open('input_'+os.path.basename(__file__).split(".")[0]+'.txt').read().splitlines()
+
+# Parse input
+
 
 
 def solve1():
