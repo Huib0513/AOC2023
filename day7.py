48576da1fb86d8462a06eacd8901dafb53b11c26
Huib van Nieuwenhuijze <Huib.van.Nieuwenhuijze@gmail.com>
2023-12-07 09:13:24 +0100

    day 7 start

--- day7.py
+++ day7.py
@@ -19,16 +19,34 @@ input = ["32T3K 765",
 # input complete lines
 #input = open('input_'+os.path.basename(__file__).split(".")[0]+'.txt').read().splitlines()
 
+cardorder = ['A', 'K', 'Q', 'J', 'T', '9', '8', '7', '6', '5', '4', '3', '2']
+typeorder = {'fiveofakind': 1, 'fourofakind': 2, 'fullhouse': 3, 'threeofakind': 4, 'twopair': 5, 'onepair': 6, 'highcard': 7}
+
 def get_type(hand):
     c = Counter(hand)
-    if len(c) == 1:
-        return 
+    match max(c.values()):
+        case 5:
+            return 'fiveofakind'
+        case 4: 
+            return 'fourofakind'
+        case 1: 
+            return 'highcard'
+        case 2: 
+            if len(c) == 3: 
+                return'twopair'
+            else:
+                return 'onepair'
+        case 3: 
+            if len(c) == 3:
+                return 'threeofakind'
+            else:
+                return 'fullhouse'
 
 
 def solve1():
-    cardorder = ['A', 'K', 'Q', 'J', 'T', '9', '8', '7', '6', '5', '4', '3', '2']
-    hands = [l.split[0] for l in input]
-    bets = [l.split[1] for l in input]
+    hands = {l.split()[0]:typeorder[get_type(l.split()[0])] for l in input}
+    bets = [l.split()[1] for l in input]
+    print(hands)
 
     print("Deel 1: No")
 
