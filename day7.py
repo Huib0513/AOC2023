615a1224e79d2e93c2fc58537118e29fdbf3fb9d
Huib van Nieuwenhuijze <Huib.van.Nieuwenhijze@gmail.com>
2023-12-07 08:28:52 +0100

    Day 7 setp

--- day7.py
+++ day7.py
@@ -1,9 +1,14 @@
 #!python3
 import os
 import datetime
+from collections import Counter
 
 # Test input
-input = [""]
+input = ["32T3K 765",
+"T55J5 684",
+"KK677 28",
+"KTJJT 220",
+"QQQJA 483"]
 
 # input integers separated by commas
 #input= [int(i) for i in open("input_'+os.path.basename(__file__).split(".")[0]+'.txt').read().split(",")]
@@ -14,8 +19,17 @@ input = [""]
 # input complete lines
 #input = open('input_'+os.path.basename(__file__).split(".")[0]+'.txt').read().splitlines()
 
+def get_type(hand):
+    c = Counter(hand)
+    if len(c) == 1:
+        return 
+
 
 def solve1():
+    cardorder = ['A', 'K', 'Q', 'J', 'T', '9', '8', '7', '6', '5', '4', '3', '2']
+    hands = [l.split[0] for l in input]
+    bets = [l.split[1] for l in input]
+
     print("Deel 1: No")
 
 def solve2():
