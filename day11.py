3f09f59851914050888413a7a3cef60c3234d77f
Huib van Nieuwenhuijze <Huib.van.Nieuwenhijze@gmail.com>
2023-12-11 20:42:13 +0100

    solve day 11

--- day11.py
+++ day11.py
@@ -23,7 +23,7 @@ input = [
 #input = [int(i) for i in open('input_'+os.path.basename(__file__).split(".")[0]+'.txt').read().splitlines()]
 
 # input complete lines
-#input = open('input_'+os.path.basename(__file__).split(".")[0]+'.txt').read().splitlines()
+input = open('input_'+os.path.basename(__file__).split(".")[0]+'.txt').read().splitlines()
 
 def get_lengths(coordinates):
     result = 0
@@ -31,34 +31,38 @@ def get_lengths(coordinates):
         result = 0
     else:
         result = get_lengths(coordinates[1:])
-        curlength= sum([(abs(coordinates[0][0])-abs(c[0])) + (abs(coordinates[0][1]) - abs(c[1])) for c in coordinates[1:]])
-        result += curlength #sum([(abs(coordinates[0][0])-abs(c[0])) + (abs(coordinates[1]) - abs(c[1])) for c in coordinates[1:]])
+        curlengths = [abs(coordinates[0][0]-c[0]) + abs(coordinates[0][1] - c[1]) for c in coordinates[1:]]
+        result += sum(curlengths) #sum([(abs(coordinates[0][0])-abs(c[0])) + (abs(coordinates[1]) - abs(c[1])) for c in coordinates[1:]])
+    return result
 
-def solve1():
+def solve1(expansion):
     result, expanded_row = 0, 0
     galaxies, empty_columns = [], []
 
     for col in range(len(input[0])):
         if (len([input[row][col] for row in [x for x in range(len(input))] if input[row][col] == '#']) == 0):
             empty_columns.append(col)
-    print(empty_columns)
+    #print(empty_columns)
 
     for r in range(len(input)):
         if (input[r].find('#') == -1):
-            expanded_row += 1
+            expanded_row += expansion
         else:
             for g in [(expanded_row,col) for col in [c for c in range(len(input[r])) if input[r][c] == '#'] ]:
                 galaxies.append(g)
         expanded_row += 1
-    print(galaxies)
+    #print(galaxies)
 
     # shift columns
-    for shift,c in enumerate(empty_columns, 1):
+    for c in sorted(empty_columns, reverse = True):
+        newgalaxies = []
         for g in galaxies:
             if g[1] > c:
-                galaxies.remove(g)
-                galaxies.append((g[0], g[1]+shift))
-    print(galaxies)
+                newgalaxies.append((g[0], g[1]+expansion))
+            else:
+                newgalaxies.append(g)
+        galaxies = newgalaxies
+    #print(galaxies)
 
     print("Deel 1: " + str(get_lengths(galaxies)))
 
@@ -66,9 +70,9 @@ def solve2():
     print("Deel 2: No")
 
 start = datetime.datetime.now()
-solve1()
+solve1(1)
 print("Tijd voor oplossing 1: ", datetime.datetime.now()-start)
 
 start = datetime.datetime.now()
-solve2()
+solve1(999999)
 print("Tijd voor oplossing 2: ", datetime.datetime.now()-start)
